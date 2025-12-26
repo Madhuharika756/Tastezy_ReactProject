c5971a04f3fc1a064425c19881f26fc55e5a5ad2
Madhuharika756 <madhuharikac@gmail.com>
2025-12-26 12:25:33 +0530

    Tastezy-food-explorer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     <>
       <div className="flex flex-col md:flex-row justify-between bg-amber-400 items-center px-4 py-2 sticky top-0 z-50">
 
-        
+
         <div className="w-full flex justify-between items-center my-3 md:my-0">
           <div className="logo flex items-center gap-2">
             <img
@@ -46,9 +46,18 @@ const Header = () => {
             <li className="transition-transform duration-300 hover:scale-115 hover:text-white">
               <Link to="/about">About</Link>
             </li>
-            <li className="transition-transform duration-300 hover:scale-115 hover:text-white">
-              <Link to="/cart">Cart ({cartitems.length})</Link>
+            <li>
+              <Link
+                to="/cart"
+                className="flex items-center gap-1 transition-transform duration-300 hover:scale-115 hover:text-white"
+              >
+                <span>🛒</span>
+                <span className="text-md font-semibold">
+                  {cartitems.length}
+                </span>
+              </Link>
             </li>
+
           </ul>
         </div>
 
