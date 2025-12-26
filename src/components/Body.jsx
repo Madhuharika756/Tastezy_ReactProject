0d3854baf2efafa7e814bce2e7fdbf0956d4f4c3
Madhuharika756 <madhuharikac@gmail.com>
2025-12-26 11:46:21 +0530

    Tastezy-Food-Explorer

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -36,21 +36,24 @@ const Body = () => {
     <>
       <div className="top-section my-4 flex flex-wrap justify-center">
         <div className="search-section mx-5 ">
-          <input type="text" className="search w-2xl mr-3 p-2 bg-gray-200 rounded-xl" placeholder="Search Food Intem" value={searchText} onChange={(e) => {
-            setSearchtext(e.target.value);
-          }} />
-          <button className="bg-orange-400 text-white font-medium px-4 py-2 rounded-md hover:bg-amber-600 cursor-pointer"
-            onClick={() => {
-              const filteredData = allRestaurant.filter((res) => res?.info?.name?.toLowerCase().includes(searchText.toLowerCase()));
-              console.log(filteredData);
-              setListOfRestaurant(filteredData);
-            }} >Search</button>
+          <div className="flex flex-col md:flex-row gap-2">
+
+            <input type="text" className="w-full md:w-96 mb-2 md:mb-0 mr-0 md:mr-3 p-2 bg-gray-200 rounded-xl" placeholder="Search Food Intem" value={searchText} onChange={(e) => {
+              setSearchtext(e.target.value);
+            }} />
+            <button className="bg-orange-400 text-white font-medium px-4 py-2 rounded-md hover:bg-amber-600 cursor-pointer"
+              onClick={() => {
+                const filteredData = allRestaurant.filter((res) => res?.info?.name?.toLowerCase().includes(searchText.toLowerCase()));
+                console.log(filteredData);
+                setListOfRestaurant(filteredData);
+              }} >Search</button>
+            <button className="bg-orange-400 text-white font-medium px-4 py-2 rounded-md hover:bg-amber-600 cursor-pointer"
+              onClick={() => {
+                const filteredData = allRestaurant.filter((res) => res.info.avgRating > 4.4);
+                setListOfRestaurant(filteredData);
+              }}>Top Rated Food Item</button>
+          </div>
         </div>
-        <button className="bg-orange-400 text-white font-medium px-4 py-2 rounded-md hover:bg-amber-600 cursor-pointer"
-          onClick={() => {
-            const filteredData = allRestaurant.filter((res) => res.info.avgRating > 4.4);
-            setListOfRestaurant(filteredData);
-          }}>Top Rated Food Item</button>
       </div>
       <div className="restaurantContainer flex flex-wrap justify-center">
         {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,15 +9,16 @@ const Header = () => {
   // console.log(cartitems);
   return (
     <>
-      <div className=" flex justify-between bg-amber-400 items-center">
-        <div className="logo">
-          <img src={LOGO_URL} alt="logo-image" className="w-18 rounded-full mx-6" />
+      <div className=" flex flex-col md:flex-row justify-between bg-amber-400 items-center px-4 py-2 sticky top-0 z-50">
+        <div className="logo flex items-center gap-2 my-3 md:my-0">
+          <img src={LOGO_URL} alt="logo-image" className="w-12 md:w-14 rounded-full mr-3"/>
+        <h1 className="text-xl md:text-3xl font-semibold text-center md:text-left">Good Food, Good mood!</h1>
         </div>
         <div className="nav-bar ">
-          <ul className="flex text-xl font-semibold">
-            <li className="mr-5.5 transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/"}>Home</Link></li>
-            <li className="mr-5.5 transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/about"}>About</Link></li>
-            <li className="mr-5.5 transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/cart"}>
+          <ul className="flex gap-6 text-lg md:text-xl font-semibold items-center my-3 md:my-0">
+            <li className=" transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/"}>Home</Link></li>
+            <li className=" transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/about"}>About</Link></li>
+            <li className=" transition-transform duration-300 hover:scale-115 hover:text-white"><Link to={"/cart"}>
              Cart ({cartitems.length})</Link></li>
           </ul>
         </div>
